cc278002d3f4b69204a4c2d9e7a760ba4e800933
Endang Ismaya <endang.ismaya@gmail.com>
2023-12-17 21:25:19 +0700

    first db migration

--- app_profile_api/models.py
+++ app_profile_api/models.py
@@ -1,5 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        # normalize email
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        # hashed password
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email=email, name=name, password=password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 # create a custom user model
