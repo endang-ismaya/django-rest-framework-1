312a7021ca6b0cdabf4b3ac26b280cdc17c56df3
Endang Ismaya <endang.ismaya@gmail.com>
2023-12-18 08:41:48 +0700

    Add ViewSet methods to profile_api

--- app_profile_api/urls.py
+++ app_profile_api/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from app_profile_api import views
 
-urlpatterns = [path("hello-view/", views.HelloAPIView.as_view())]
+router = DefaultRouter()
+router.register(prefix="hello-viewset", viewset=views.HelloViewSet, basename="hello-viewset")
+
+urlpatterns = [
+    path("hello-view/", views.HelloAPIView.as_view()),
+    path("", include(router.urls)),
+]
