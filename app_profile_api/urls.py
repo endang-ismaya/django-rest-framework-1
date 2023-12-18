0dac2fc44125cd8f3c991badcf575056b1c0d1f6
Endang Ismaya <endang.ismaya@gmail.com>
2023-12-18 09:53:25 +0700

    add login api

--- app_profile_api/urls.py
+++ app_profile_api/urls.py
@@ -16,5 +16,6 @@ router.register(
 
 urlpatterns = [
     path("hello-view/", views.HelloAPIView.as_view()),
+    path("login/", views.UserLoginApiView.as_view()),
     path("", include(router.urls)),
 ]
